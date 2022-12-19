48cf3a9c6ff9f022f8780950b3048c3fa7c74042
Gabriel Rodríguez Domínguez <52460428+gatroxrd@users.noreply.github.com>
2022-12-19 13:27:48 -0600

    Update descargaScriptSESEAP.bat

--- descargaScriptSESEAP.bat
+++ descargaScriptSESEAP.bat
@@ -1,8 +1,5 @@
 @ECHO OFF
-wget -c -S -r https://raw.githubusercontent.com/gatroxrd/API-S1-SESEAP-Puebla-Windows/main/instalaSESEAP_S1_Windows.ps1
-
-ECHO Invoke-WebRequest "https://raw.githubusercontent.com/gatroxrd/API-S1-SESEAP-Puebla-Windows/main/instalaSESEAP_S1_Windows.ps1" -OutFile "C:\Users\%USERNAME%\ai\exdown.exe">>"%PSScript%"
-
+@ECHO Descargando script de instalación de la API de la Secretaría Ejecutiva del Sistema Estatal Anticorrupión Puebla
+Invoke-WebRequest "https://raw.githubusercontent.com/gatroxrd/API-S1-SESEAP-Puebla-Windows/main/instalaSESEAP_S1_Windows.ps1" -OutFile "%userprofile%\Desktop\instalaSESEAP_S1_Windows.ps1"
 echo continuar ...
-
 pause > nul
